b3766efd26c765768c1f61c1dea84128099ec102
DevSide <DevSide@github.com>
2020-03-03 17:21:22 +0100

    feat: node 13.8.0

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker build . --tag "devside/node-gcloud:13.6.0" -f - < ./Dockerfile
+docker build . --tag "devside/node-gcloud:13.8.0" -f - < ./Dockerfile
--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 sh ./build.sh
 docker login
-docker tag devside/node-gcloud:13.6.0 devside/node-gcloud:13.6.0
-docker push devside/node-gcloud:13.6.0
+docker tag devside/node-gcloud:13.8.0 devside/node-gcloud:13.8.0
+docker push devside/node-gcloud:13.8.0
